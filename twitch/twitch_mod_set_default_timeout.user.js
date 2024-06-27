2c385c5a0ead9aa5ce1503f973b679957958c8bf
x0x0b <42596409+x0x0b@users.noreply.github.com>
2024-06-27 21:42:33 +0900

    fix

--- twitch/twitch_mod_set_default_timeout.user.js
+++ twitch/twitch_mod_set_default_timeout.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Set Custom Ban Time 1 Day
 // @namespace    Violentmonkey Scripts
-// @version      0.1
+// @version      0.2
 // @description  Set initial value for custom ban time to 1 day
 // @match        https://www.twitch.tv/moderator/*
 // @grant        none
@@ -10,10 +10,7 @@
 (function () {
   "use strict";
 
-  function setInputValue() {
-    let inputElement = document.querySelector(
-      'input[placeholder="カスタムの発言禁止時間を設定"]'
-    );
+  function setInputValue(inputElement) {
     if (
       inputElement &&
       inputElement.offsetParent !== null &&
@@ -23,9 +20,31 @@
     }
   }
 
-  document.addEventListener("click", function () {
-    setTimeout(setInputValue, 100);
-  });
+  function observeDOM() {
+    const targetNode = document.body;
+    const config = { childList: true, subtree: true };
 
-  window.addEventListener("load", setInputValue);
+    const callback = function (mutationsList, observer) {
+      for (let mutation of mutationsList) {
+        if (mutation.type === "childList") {
+          let inputElement = document.querySelector(
+            'input[placeholder="カスタムの発言禁止時間を設定"]'
+          );
+          if (inputElement) {
+            setInputValue(inputElement);
+          }
+        }
+      }
+    };
+
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+  }
+
+  // DOMの読み込みが完了したら監視を開始
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", observeDOM);
+  } else {
+    observeDOM();
+  }
 })();
